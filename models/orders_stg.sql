cdc42204b5367dcc374fff960e18bc0505a2c140
raghutapal <tapal.raghu@gmail.com>
2024-03-18 18:38:01 +0000

    Feature1 Changes

--- models/orders_stg.sql
+++ models/orders_stg.sql
@@ -8,6 +8,6 @@ STATUS,
 CASE WHEN STATUS = 'Shipped' THEN 'Order is Shipped'
      WHEN STATUS = 'Pending' THEN 'Order is in pending state'
      WHEN STATUS = 'Processing' THEN 'Order is being processed'
-     WHEN STATUS = 'Delivered' THEN 'Order is delivered' ELSE 'Some other status' END AS STATUSDESC,
+     WHEN STATUS = 'Delivered' THEN 'Order is delivered' ELSE 'Some unknown status' END AS STATUSDESC,
 UPDATED_AT
 FROM ORDERS
